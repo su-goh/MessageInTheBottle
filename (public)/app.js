371a30a9eb501289166a4bf10e7a5803026417a8
su-goh <49136637+su-goh@users.noreply.github.com>
2020-08-30 14:31:12 -0500

    Integrated with firebase
    Integrated with firebase database and removal of php

---
+++ (public)/app.js
@@ -0,0 +1,12 @@
+const msg = document.getElementById('msgBox');
+const submitBtn = document.getElementById('submitBtn');
+
+const database = firebase.database();
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    database.ref('/msg/' + msg.value).set({
+        msg: msg.value
+    })
+    document.getElementById('msgForm').reset();
+})
