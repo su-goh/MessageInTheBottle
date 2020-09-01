f61e03bd00f2ccea731e1a3d8ce9ee279b9fe8f6
su-goh <49136637+su-goh@users.noreply.github.com>
2020-09-01 00:43:42 -0500

    Enter on submit
    - Added enter to submit functionality
    - Punctuations now work
    - Added autoID for messages
    - created method for sendMessage for better code reusability

--- (public)/app.js
+++ (public)/app.js
@@ -2,27 +2,31 @@ const msg = document.getElementById('msgBox');
 const submitBtn = document.getElementById('submitBtn');
 
 const database = firebase.database();
+const rootRef = database.ref('msgs');
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    database.ref('/msg/' + msg.value).set({
-        msg: msg.value
-    })
-    document.getElementById('msgForm').reset();
-    window.alert("Your message is floating in the ocean.")
+    submitMessage();
 })
 
 document.getElementById('msgBox').addEventListener("keydown", function(e) {
     if (!e) {
         var e = window.event;
     }
-    
+
     e.preventDefault;
     if(e.keyCode == 13) {
-        database.ref('/msg/' + msg.value).set({
-            msg: msg.value
-        })
-        document.getElementById('msgForm').reset();
-        window.alert("Your message is floating in the ocean.")
+        submitMessage();
     }
+    
 }, false);
+
+const submitMessage = () => {
+    const autoId = rootRef.push().key;
+    rootRef.child(autoId).set ({
+        msg: msg.value
+    });
+
+    document.getElementById('msgForm').reset();
+    window.alert("Your message is floating in the ocean.");
+}
