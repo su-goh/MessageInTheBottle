93a1e2818294b6d4d6c6c00d5b1a3617879416ce
su-goh <49136637+su-goh@users.noreply.github.com>
2020-08-31 23:37:15 -0500

    CSS Improvements
    - Removed outline for textbox
    - Added enter to submit function

--- (public)/app.js
+++ (public)/app.js
@@ -11,3 +11,18 @@ submitBtn.addEventListener('click', (e) => {
     document.getElementById('msgForm').reset();
     window.alert("Your message is floating in the ocean.")
 })
+
+document.getElementById('msgBox').addEventListener("keydown", function(e) {
+    if (!e) {
+        var e = window.event;
+    }
+    
+    e.preventDefault;
+    if(e.keyCode == 13) {
+        database.ref('/msg/' + msg.value).set({
+            msg: msg.value
+        })
+        document.getElementById('msgForm').reset();
+        window.alert("Your message is floating in the ocean.")
+    }
+}, false);
