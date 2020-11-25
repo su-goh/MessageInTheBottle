5cc2c41db25dddb3783b66444d3566679143a49b
su-goh <49136637+su-goh@users.noreply.github.com>
2020-11-24 22:01:39 -0600

    Update with retrieve data and add card

--- (public)/app.js
+++ (public)/app.js
@@ -3,6 +3,44 @@ const submitBtn = document.getElementById('submitBtn');
 
 const database = firebase.database();
 const rootRef = database.ref('msgs');
+var ref = database.ref('msgs');
+ref.on('value', getData, errHandle)
+
+function getData(data) {
+    var msg = data.val();
+    var keys = Object.keys(msg);
+    var value = Object.values(msg);
+
+    console.log(value);
+
+    value.forEach(element => {
+        console.log(element.msg);
+
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.style = 'width: 18rem';
+
+        let cardBody = document.createElement('div');
+        cardBody.className ='card-body';
+
+        let message = document.createElement('P');
+        message.innerHTML = element.msg;
+        message.style = 'card-text';
+
+        cardBody.appendChild(message);
+        card.appendChild(cardBody);
+
+        document.getElementById("cards").appendChild(card);
+    });
+
+
+
+
+}
+
+function errHandle(err) {
+    console.log(err);
+}
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -15,9 +53,10 @@ document.getElementById('msgBox').addEventListener("keydown", function(e) {
     }
 
     e.preventDefault;
-    if(e.keyCode == 13) {
-        submitMessage();
-    }
+    // if press enter key, submit message
+    // if(e.keyCode == 13) {
+    //     submitMessage();
+    // }
     
 }, false);
 
